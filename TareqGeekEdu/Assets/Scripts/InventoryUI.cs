780865318fb7ab05a15f66c7755e47ca1ec346ee
NateWhitehead96 <50887818+NateWhitehead96@users.noreply.github.com>
2022-01-20 17:09:48 -0500

    Added lose screen

--- TareqGeekEdu/Assets/Scripts/InventoryUI.cs
+++ TareqGeekEdu/Assets/Scripts/InventoryUI.cs
@@ -12,10 +12,13 @@ public class InventoryUI : MonoBehaviour
     public Image LifeThree;
 
     public PlayerScript player; // we have access to our player script for the health
+
+    public GameObject GameOverCanavs;
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerScript>(); // this makes sure the player is the one from the scene
+        GameOverCanavs.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,6 +34,10 @@ public class InventoryUI : MonoBehaviour
         {
             LifeTwo.rectTransform.rotation = Quaternion.Euler(0, 0, 180); // when we are lower then 3 health, rotate the 3rd heart upside down
         }
-
+        if(player.Health <= 0)
+        {
+            Time.timeScale = 0;
+            GameOverCanavs.SetActive(true);
+        }
     }
 }
